24ea2828ada21ad3527fe77dae9cc1ad4978d47b
Vova Kapustynskyi <volodymyr.kapustynskyi@kindgeek.com>
2022-10-14 23:56:24 +0300

    Add first components from angular tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from "../shared/hero";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Windstorm'},
+  {id: 2, name: 'Shark'},
+  {id: 3, name: 'Lion'},
+  {id: 4, name: 'Tornado'},
+  {id: 5, name: 'Dark'}
+]
